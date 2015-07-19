0c687c80ed65406e93c1bdb84a766bf009cfebdd
colleenmcguckin <colleenmcguckin@gmail.com>
2015-07-19 15:05:28 -0500

    all search and filter functions now operational

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,20 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    
+
+    if params[:keysignature] && params[:timesignature]
+      @songs = Song.where(key_id: params[:keysignature], time_signature_id: params[:timesignature])
+    elsif params[:keysignature]
+      @songs = Song.where(key_id: params[:keysignature])
+    elsif params[:timesignature]
+      @songs = Song.where(time_signature_id: params[:timesignature]) 
+    elsif params[:song_name]
+      @songs = Song.where("name LIKE (?)",  "%#{params[:song_name]}%" )
+    else 
+      @songs = Song.all
+    end
+
+   
   end
 
   def new
