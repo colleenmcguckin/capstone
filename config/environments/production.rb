1aedc286f632cdee7005fb9ff1324754b3644575
colleenmcguckin <colleenmcguckin@gmail.com>
2015-08-05 17:50:49 -0500

    added to application.rb to get around uglifier for js minification turn off

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
 
+  config.assets.js_compressor = Uglifier.new(mangle: false)
+
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
